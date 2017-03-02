4d3de1a0f9ea779a17c1956603e2f560608c67dd
Noah Lee <nol006@ucsd.edu>
2017-03-02 00:09:37 -0800

    thirdpartyAPI lab

---
+++ public/js/googleMaps.js
@@ -0,0 +1,16 @@
+function initMap() {
+  // Create center marker at UCSD
+  var ucsd_ltlng = {lat:32.884993, lng:-117.241287};
+
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: ucsd_ltlng,
+    zoom: 18
+  });
+
+  var marker = new google.maps.Marker({
+      position: ucsd_ltlng,
+      map: map,
+      title: 'UCSD'
+  });
+}
